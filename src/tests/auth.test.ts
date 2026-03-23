e7194dde224661a2fbdb2af61044ec5541747651
Hamzeh <hamzehjarrar604@gmail.com>
2026-03-23 17:56:00 +0200

    format code with prettier

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,10 +2,9 @@ import { describe, test, expect } from "vitest";
 import { getAPIKey } from "../api/auth.js";
 
 describe("getAPIKey", () => {
-
   test("returns API key if exists", () => {
     const headers = {
-      authorization: "ApiKey 123456"
+      authorization: "ApiKey 123456",
     };
 
     const result = getAPIKey(headers as any);
@@ -20,5 +19,4 @@ describe("getAPIKey", () => {
 
     expect(result).toBeNull();
   });
-
 });
