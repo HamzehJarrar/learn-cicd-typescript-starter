daae358aa506ed9479dddec04ad979740329d291
Hamzeh <hamzehjarrar604@gmail.com>
2026-03-23 18:32:39 +0200

    fixing auth test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,21 +1,22 @@
 import { describe, test, expect } from "vitest";
+import type { IncomingHttpHeaders } from "http";
 import { getAPIKey } from "../api/auth.js";
 
 describe("getAPIKey", () => {
   test("returns API key if exists", () => {
-    const headers = {
+    const headers: IncomingHttpHeaders = {
       authorization: "ApiKey 123456",
     };
 
-    const result = getAPIKey(headers as any);
+    const result = getAPIKey(headers);
 
     expect(result).toBe("123456");
   });
 
   test("returns null if no API key", () => {
-    const headers = {};
+    const headers: IncomingHttpHeaders = {};
 
-    const result = getAPIKey(headers as any);
+    const result = getAPIKey(headers);
 
     expect(result).toBeNull();
   });
